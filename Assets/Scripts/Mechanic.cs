423dc04912765678c7500b9697830ff92919841b
ferend <dalcikferhateren@gmail.com>
2022-11-27 18:29:56 +0300

    create env, add control

---
+++ Assets/Scripts/Mechanic.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+    public abstract class Mechanic : MonoBehaviour
+    {
+
+
+        public virtual IEnumerator OnStart()
+        {
+            yield break;
+        }
+
+        public virtual IEnumerator OnEnd()
+        {
+            yield break;
+        }
+
+        public virtual IEnumerator OnFail()
+        {
+            yield break;
+        }
+
+        public virtual void OnDown()
+        {
+        }
+
+        public virtual void OnDrag()
+        {
+        }
+
+        public virtual void OnUp()
+        {
+        }
+        
+    }
